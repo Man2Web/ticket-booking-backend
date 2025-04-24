7bad48a1cd3c9fb4ed26c5c23816f8201063b9de
Harsha-Vardhan-24 <javvajiharshavardhan.24@gmail.com>
2025-04-24 12:29:01 +0530

    fix: fixed few issues and modified the serverless function a little

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
 const serverless = require("serverless-http");
-// const createApp = require("./src/app");
-
-// let cachedApp;
-
-// module.exports.handler = async (event, context) => {
-//   if (!cachedApp) {
-//     const app = await createApp();
-//     cachedApp = serverless(app);
-//   }
-//   return cachedApp(event, context);
-// };
 const app = require("./src/app");
-serverless.exports = app;
+module.exports.handler = serverless(app);
