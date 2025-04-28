4c5b1e667b9eae63a8dac42ce21b5d61a6986972
Harsha-Vardhan-24 <javvajiharshavardhan.24@gmail.com>
2025-04-28 11:03:54 +0530

    fix: fixed cors error

--- src/app.js
+++ src/app.js
@@ -24,7 +24,18 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Access-Control-Allow-Credentials",
+    ],
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
